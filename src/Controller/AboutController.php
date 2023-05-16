f22f735b0baaf6bb2b120d8bd3e740ab9e25e946
Juan Manuel Sanchez <jm@MacBook-Pro.local>
2023-05-16 12:06:24 +0200

    adding controllers

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class NewsController extends AbstractController
+class AboutController extends AbstractController
 {
     /**
-     * @Route("/news", name="app_news")
+     * @Route("/about", name="app_about")
      */
     public function index(): Response
     {
-        return $this->render('news/index.html.twig', [
-            'controller_name' => 'NewsController',
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'AboutController',
         ]);
     }
 }
--- src/Controller/WorksController.php
+++ src/Controller/WorksController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class NewsController extends AbstractController
+class WorksController extends AbstractController
 {
     /**
-     * @Route("/news", name="app_news")
+     * @Route("/works", name="app_works")
      */
     public function index(): Response
     {
-        return $this->render('news/index.html.twig', [
-            'controller_name' => 'NewsController',
+        return $this->render('works/index.html.twig', [
+            'controller_name' => 'WorksController',
         ]);
     }
 }
